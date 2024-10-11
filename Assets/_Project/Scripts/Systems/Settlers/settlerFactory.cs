ade759bc8def71062e61e3d78e21ec3dc59e94ac
Don MacSween <dm1200@student.aru.ac.uk>
2024-10-11 17:15:34 +0100

    Basic Stats completed

---
+++ Assets/_Project/Scripts/Systems/Settlers/settlerFactory.cs
@@ -0,0 +1,17 @@
+/* Author       : Don MacSween
+ * email        : dm1200@student.aru.ac.uk
+ * License      : CC BY 4.0 https://creativecommons.org/licenses/by/4.0/
+ * Last Modified: 11/10/2024
+ * Purpose : This script is designed as sole method that components can use to create a settler.
+ *           The create method can be overloaded to allow for different configurations of settler to be created.
+ */
+
+using UnityEngine;
+namespace Settlers
+{
+    public class settlerFactory : MonoBehaviour
+    {
+     
+    }
+}
+
