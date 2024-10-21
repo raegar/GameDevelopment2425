9bcd83e3082f87ab9dcbbf885792e20d173dfb73
Jess <jw1519@student.aru.ac.uk>
2024-10-21 18:14:09 +0100

    added inventory manager script

--- Assets/_Project/Scripts/Systems/Inventory/InventoryManager.cs
+++ Assets/_Project/Scripts/Systems/Inventory/InventoryManager.cs
@@ -1,18 +1,48 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class InventoryManager : MonoBehaviour
+namespace InventorySystem
 {
-    // Start is called before the first frame update
-    void Start()
+    public class InventoryManager : MonoBehaviour
     {
-        
-    }
+        public static InventoryManager instance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public List<Item> itemList = new List<Item>();
+        public Transform itemContainer;
+        public GameObject itemTemplate;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        public void AddItem(Item item)
+        {
+            itemList.Add(item);
+        }
+
+        public void ListItems()
+        {
+            foreach (Item item in itemList)
+            {
+                GameObject gameObject = Instantiate(itemTemplate, itemContainer);
+
+                var itemImage = gameObject.transform.Find("itemImage");
+
+                if (itemImage != null)
+                {
+                    Image image = itemImage.GetComponent<Image>();
+                    image.sprite = item.itemSprite;
+                }
+                var itemName = gameObject.transform.Find("itemName");
+                if (itemName != null)
+                {
+                    Text text = itemName.GetComponent<Text>();
+                    text.text = item.itemName;
+
+                }
+            }
+        }
     }
 }
