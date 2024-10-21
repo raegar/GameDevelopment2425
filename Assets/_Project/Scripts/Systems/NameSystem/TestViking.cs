d8f57d1a75199b3575a19c1f8b6e17f54dec1eb4
ID274 <id274@student.aru.ac.uk>
2024-10-21 22:17:40 +0100

    Got rid of tuple in the TestViking script as well

--- Assets/_Project/Scripts/Systems/NameSystem/TestViking.cs
+++ Assets/_Project/Scripts/Systems/NameSystem/TestViking.cs
@@ -2,8 +2,8 @@
 /* Author  : Ignacy | https://github.com/ID274
  * License : CC BY 4.0 https://creativecommons.org/licenses/by/4.0/
  * Purpose : This script is a test script used to demonstrate the naming system in action.
- *           It stores the viking's name as a tuple and extracts the first and last names from it, allowing use of multiple types.
- *           To use this script, attach it to a GameObject in the scene and assign the isMale, and isChild variables in the inspector.       
+ *           It stores the viking's name as a string and extracts the first and last names from it.
+ *           To use this script, attach it to a GameObject in the scene and assign values in the inspector, or use the NameSystemTest scene.       
  */
 
 using TMPro;
@@ -11,12 +11,8 @@ using UnityEngine;
 
 public class TestViking : MonoBehaviour
 {
-    //The reason we store the vikingName as a tuple is to have easily access to both the surname and the first name. You can deconstruct it
-    //into two separate strings like we do in start. This results in us having 1 tuple, as well as 2 strings one for first and one for last name.
+    [SerializeField] private string vikingFullName;
 
-    [SerializeField] private (string, string) vikingName; // <--- vikingName stored as a tuple
-
-    //First and Last names extracted from the vikingName tuple.
     [SerializeField] private string vikingFirstName, vikingLastName, fatherName;
     [SerializeField] private bool isChild, usePatronymics;
     [SerializeField] private TextMeshPro nameText; // <--- TextMeshPro component to display the viking's name.
@@ -24,9 +20,11 @@ public class TestViking : MonoBehaviour
 
     private void Start()
     {
-        vikingName = NamingManager.Instance.GetFullNameQuickly(gender, isChild, usePatronymics, fatherName); // <--- The method called here provides complete functionality for getting a full name.
-        (vikingFirstName, vikingLastName) = vikingName; // <--- Deconstruct the tuple into two separate strings.
-        nameText.text = vikingFirstName + " " + vikingLastName; // <--- Display the viking's name in the scene.
-        Debug.Log(vikingName);
+        vikingFullName = NamingManager.Instance.GetFullNameQuickly(gender, isChild, usePatronymics, fatherName); // <--- The method called here provides complete functionality for getting a full name.
+        string[] nameSplit = vikingFullName.Split(" "); // <--- Split the full name into first and last names.
+        vikingFirstName = nameSplit[0];
+        vikingLastName = nameSplit[1];
+        nameText.text = vikingFullName; // <--- Display the viking's name in the scene.
+        Debug.Log(vikingFullName);
     }
 }
