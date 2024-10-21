1996040e8869cec593024d4be32c9a111508fb4e
Jess <jw1519@student.aru.ac.uk>
2024-10-21 17:47:54 +0100

    made a scripable object item

--- Assets/_Project/Scripts/Systems/Inventory/interface/IStackable.cs
+++ Assets/_Project/Scripts/Systems/Inventory/interface/IStackable.cs
@@ -6,7 +6,6 @@ namespace InventorySystem
 {
     public interface IStackable
     {
-        public Transform parent { get; }
         public void AddToStack(string itemToAdd, int amountToAdd, int currentAmount, int maxAmount);
     }
 }
--- Assets/_Project/Scripts/Systems/Inventory/item.cs
+++ Assets/_Project/Scripts/Systems/Inventory/item.cs
@@ -1,28 +1,12 @@
-//using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace InventorySystem
 {
-    public class Item : MonoBehaviour
+    [CreateAssetMenu (fileName = "New item", menuName = "create new item")]
+    public class Item : ScriptableObject
     {
-        //item details
         public string itemName;
-        //public TextMeshProUGUI itemNameText;
-
-        public Sprite itemSprite;
-        public Image itemImage;
         public int amount;
-
-        private void Start()
-        {
-            // itemName = gameObject.name;
-            // itemNameText = GetComponentInChildren<TextMeshProUGUI>();
-            //itemNameText.text = itemName; // set the item name
-
-            itemImage = GetComponentInChildren<Image>();
-            itemImage.sprite = itemSprite; // set the item sprite
-
-        }
+        public Sprite itemSprite;
     }
 }
