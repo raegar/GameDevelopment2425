bf8ee698b145d3295e17e1e72f72d9ddb14011a7
Don MacSween <dm1200@student.aru.ac.uk>
2024-10-22 20:53:14 +0100

    EdgeScrolling Complete

--- Assets/_Project/Scripts/Systems/Camera/GameInput.cs
+++ Assets/_Project/Scripts/Systems/Camera/GameInput.cs
@@ -6,21 +6,22 @@ using UnityEngine.EventSystems;
 public class GameInput : MonoBehaviour , IPointerMoveHandler,IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] private float moveSpeed = 50f;
+    [SerializeField] private bool useEdgeScroll = true;
     private Vector2 moveDirection;
     private Vector2 lastPointerPosition;
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        moveDirection = context.ReadValue<Vector2>();
-        Debug.Log("Move Camera");
-        Vector3 moveDir = transform.forward * context.ReadValue<Vector2>().y + transform.right * context.ReadValue<Vector2>().x;
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
-    }
 
     private void Update()
     {
         MoveCameraTarget(moveDirection);
+        if (useEdgeScroll) HandleCameraMovementEdgeScrolling();
+        
+        
     }
-
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+    
     public void MoveCameraTarget(Vector2 direction)
     {
         Vector3 moveDir = transform.forward * direction.y + transform.right * direction.x;
@@ -30,29 +31,15 @@ public class GameInput : MonoBehaviour , IPointerMoveHandler,IPointerDownHandler
     private void HandleCameraMovementEdgeScrolling()
     {
         Vector3 inputDir = new Vector3(0, 0, 0);
+        Vector2 pointerPosition = Pointer.current.position.ReadValue();
 
         int edgeScrollSize = 20;
 
-        if (Input.mousePosition.x < edgeScrollSize)
-        {
-            inputDir.x = -1f;
-        }
-        if (Input.mousePosition.y < edgeScrollSize)
-        {
-            inputDir.z = -1f;
-        }
-        if (Input.mousePosition.x > Screen.width - edgeScrollSize)
-        {
-            inputDir.x = +1f;
-        }
-        if (Input.mousePosition.y > Screen.height - edgeScrollSize)
-        {
-            inputDir.z = +1f;
-        }
-
+        if (pointerPosition.x < edgeScrollSize) {inputDir.x = -1f;}
+        if (pointerPosition.y < edgeScrollSize) {inputDir.z = -1f;}
+        if (pointerPosition.x > Screen.width - edgeScrollSize) {inputDir.x = +1f;}
+        if (pointerPosition.y > Screen.height - edgeScrollSize) {inputDir.z = +1f;}
         Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
-
-        float moveSpeed = 50f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
 
