7a518b09a8e863c65f37c2fdb4afcf38754faef1
ID274 <id274@student.aru.ac.uk>
2024-10-31 11:00:52 +0000

    Git-amend's system implemented, but I couldn't make much sense of how it works. Initial commit in case someone wants to take a look - test scene is there but not working as I don't understand the system.

---
+++ Assets/_Project/Scripts/Systems/SoundSystem/AudioPooling/Test/SFXTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXTest : MonoBehaviour
+{
+    public void OnButtonPress()
+    {
+        //... play sound
+    }
+
+    public void OnButtonPressX10()
+    {
+        //... play sound 10 times
+    }
+}
