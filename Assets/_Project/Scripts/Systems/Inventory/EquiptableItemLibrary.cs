7e553c2da2efb5059d7b6b0b1ae913cc89a48f2c
Jess <jw1519@student.aru.ac.uk>
2024-11-10 12:28:22 +0000

    edited equiptable items script

---
+++ Assets/_Project/Scripts/Systems/Inventory/EquiptableItemLibrary.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InventorySystem
+{
+    public class EquiptableItemLibrary : MonoBehaviour
+    {
+        public EquiptableItem[] equiptables;
+    }
+}
--- Assets/_Project/Scripts/Systems/Inventory/scriptableObjects/EquiptableItem.cs
+++ Assets/_Project/Scripts/Systems/Inventory/scriptableObjects/EquiptableItem.cs
@@ -7,6 +7,15 @@ namespace InventorySystem
     [CreateAssetMenu(fileName = "New item", menuName = "create item/equiptable item")]
     public class EquiptableItem : Item
     {
-
+        public int attack;
+        public int defence;
+        public void EquiptItem()
+        {
+            //update stats of viking  that want to equipt it
+        }
+        public void UnEquiptItem()
+        {
+            //update stats of viking  that want to unequipt it
+        }
     }
 }
