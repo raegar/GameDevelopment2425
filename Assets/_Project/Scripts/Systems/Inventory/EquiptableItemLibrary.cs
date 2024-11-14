656f0426cc5608d730aa88a944d0a24320182f4d
JW1519 <JW1519@student.aru.ac.uk>
2024-11-14 10:17:13 +0000

    corrected spelling

--- Assets/_Project/Scripts/Systems/Inventory/EquiptableItemLibrary.cs
+++ Assets/_Project/Scripts/Systems/Inventory/EquiptableItemLibrary.cs
@@ -1,6 +1,6 @@
 /* Author Jess
  * License : CC BY 4.0 https://creativecommons.org/licenses/by/4.0/
- * Purpose: This script handles an array of equiptable items so this can be refreced
+ * Purpose: This script handles an array of equiptable items so that the items can be refrenced
 */
 
 using UnityEngine;
