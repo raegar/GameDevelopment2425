d2c65f314c4473a729d9ab80db86c33c00deb431
JW1519 <JW1519@student.aru.ac.uk>
2024-11-14 09:55:45 +0000

    edited edible item script

--- Assets/_Project/Scripts/Systems/Inventory/scriptableObjects/EdibleItem.cs
+++ Assets/_Project/Scripts/Systems/Inventory/scriptableObjects/EdibleItem.cs
@@ -1,11 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 namespace InventorySystem
 {
-    public class EdibleItem : MonoBehaviour
+    public class EdibleItem : Item
     {
-
+        public float hunger;
     }
 }
