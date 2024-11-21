c077557f1ae59d13a830ff83b1240df6602264eb
Sacralin <PM902@student.aru.ac.uk>
2024-11-21 19:26:34 +0000

    Finished terrain manipulation

--- Assets/_Project/Scripts/Systems/Terrain/TerrainUI.cs
+++ Assets/_Project/Scripts/Systems/Terrain/TerrainUI.cs
@@ -1,112 +1,74 @@
 using sc.terrain.proceduralpainter;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
-public class TerrainUI : MonoBehaviour
+namespace terrain
 {
-
-    TerrainVertexSelector terrainVertexSelector;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class TerrainUI : MonoBehaviour
     {
-        terrainVertexSelector = GetComponent<TerrainVertexSelector>();
-    }
+        TerrainVertexSelector terrainVertexSelector;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    
-
-    public void RaiseTerrain()
-    {
-        ModifyTerrainHeight(1);
-    }
-    public void LowerTerrain()
-    {
-        ModifyTerrainHeight(-1);
-    }
-
-    //private void ModifyTerrainHeight(int value)
-    //{
-    //    TerrainPainter tPainter = GetComponent<TerrainPainter>();
-
-    //    foreach (var manipulator in terrainVertexSelector.activeManipulators)
-    //    {
-    //        foreach (var vertexData in manipulator.selectedVertices)
-    //        {
-    //            // Get Height (Stored as whole number)
-    //            float currentHeight = vertexData.vertex.y;
-    //            // Modify height value
-    //            float newHeight = currentHeight + value;
-    //            // Normalise height 
-    //            float normalizedNewHeight = newHeight / vertexData.terrain.terrainData.size.y;
-    //            // Apply new height to heightmap
-    //            vertexData.terrain.terrainData.SetHeights((int)vertexData.vertex.x, (int)vertexData.vertex.z, new float[,] { { normalizedNewHeight } });
-    //            // Reassign modified height value
-    //            vertexData.vertex.y = newHeight;
-    //        }
-
-    //        Vector3 framePosition = manipulator.frame.transform.position;
-    //        framePosition.y = framePosition.y + value;
-    //        manipulator.frame.transform.position = framePosition;
-    //        //manipulator.worldPosition = new Vector3(manipulator.worldPosition.x, manipulator.worldPosition.y + 1 , manipulator.worldPosition.z);
-    //    }
-
-    //    tPainter.RepaintAll();
-    //}
-
-    private void ModifyTerrainHeight(int value)
-    {
-        TerrainPainter tPainter = GetComponent<TerrainPainter>();
-        for (int i = 0; i < terrainVertexSelector.activeManipulators.Count; i++)
+        // Start is called before the first frame update
+        void Start()
         {
-            for (int j = 0; j < terrainVertexSelector.activeManipulators[i].selectedVertices.Count; j++)
+            terrainVertexSelector = GetComponent<TerrainVertexSelector>();
+        }
+        public void RaiseTerrain()
+        {
+            ModifyTerrainHeight(1);
+        }
+        public void LowerTerrain()
+        {
+            ModifyTerrainHeight(-1);
+        }
+
+        private void ModifyTerrainHeight(int value)
+        {
+            // Use this to clamp the max height of the frame
+            float scaledNewHeight = 0;
+            for (int i = 0; i < terrainVertexSelector.activeManipulators.Count; i++)
             {
-                // Get Height (Stored as whole number)
-                float currentHeight = terrainVertexSelector.activeManipulators[i].selectedVertices[j].vertex.y;
-                // Modify height value
-                float newHeight = currentHeight + value;
-                // Normalise height 
-                float normalizedNewHeight = newHeight / terrainVertexSelector.activeManipulators[i].selectedVertices[j].terrain.terrainData.size.y;
-                // Apply new height to heightmap
-                terrainVertexSelector.activeManipulators[i].selectedVertices[j].terrain.terrainData.SetHeights((int)terrainVertexSelector.activeManipulators[i].selectedVertices[j].vertex.x,
-                                                                                                               (int)terrainVertexSelector.activeManipulators[i].selectedVertices[j].vertex.z,
-                                                                                                               new float[,] { { normalizedNewHeight } });
-                // Reassign modified height value
-                VertexPositionData vertexPositionData = new VertexPositionData();
-                Vector3 vertexPosition = new Vector3(terrainVertexSelector.activeManipulators[i].selectedVertices[j].vertex.x,
-                                                     newHeight, 
-                                                     terrainVertexSelector.activeManipulators[i].selectedVertices[j].vertex.z);
+                for (int j = 0; j < terrainVertexSelector.activeManipulators[i].selectedVertices.Count; j++)
+                {
+                    // Get Height (Stored as whole number)
+                    float currentHeight = terrainVertexSelector.activeManipulators[i].selectedVertices[j].vertex.y;
+                    // Modify height value
+                    float newHeight = Mathf.Round(currentHeight + value);
+                    // Normalise height 
+                    float normalizedNewHeight = Mathf.Clamp(newHeight / terrainVertexSelector.activeManipulators[i].selectedVertices[j].terrain.terrainData.size.y, 0f, 1f);
 
-                vertexPositionData.SetPositionData(terrainVertexSelector.activeManipulators[i].selectedVertices[j].terrain, vertexPosition);
-                terrainVertexSelector.activeManipulators[i].selectedVertices[j] = vertexPositionData;
+
+                    // Apply new height to heightmap
+                    terrainVertexSelector.activeManipulators[i].selectedVertices[j].terrain.terrainData.SetHeights((int)terrainVertexSelector.activeManipulators[i].selectedVertices[j].vertex.x,
+                                                                                                                   (int)terrainVertexSelector.activeManipulators[i].selectedVertices[j].vertex.z,
+                                                                                                                   new float[,] { { normalizedNewHeight } });
+                    // Reassign modified height value to this list
+                    scaledNewHeight = normalizedNewHeight * terrainVertexSelector.activeManipulators[i].selectedVertices[j].terrain.terrainData.size.y;
+                    VertexPositionData vertexPositionData = new VertexPositionData();
+                    Vector3 vertexPosition = new Vector3(terrainVertexSelector.activeManipulators[i].selectedVertices[j].vertex.x,
+                                                         scaledNewHeight,
+                                                         terrainVertexSelector.activeManipulators[i].selectedVertices[j].vertex.z);
+
+                    vertexPositionData.SetPositionData(terrainVertexSelector.activeManipulators[i].selectedVertices[j].terrain, vertexPosition);
+                    terrainVertexSelector.activeManipulators[i].selectedVertices[j] = vertexPositionData;
+                }
+                // Update the frames position relative to the new height position
+                Vector3 framePosition = terrainVertexSelector.activeManipulators[i].frame.transform.position;
+                framePosition.y = scaledNewHeight;
+                terrainVertexSelector.activeManipulators[i].frame.transform.position = framePosition;
+
+                // Repaint the terrain
+                TerrainPainter tPainter = GetComponent<TerrainPainter>();
+                tPainter.RepaintAll();
             }
-
-            Vector3 framePosition = terrainVertexSelector.activeManipulators[i].frame.transform.position;
-            framePosition.y = framePosition.y + value;
-            terrainVertexSelector.activeManipulators[i].frame.transform.position = framePosition;
-            //manipulator.worldPosition = new Vector3(manipulator.worldPosition.x, manipulator.worldPosition.y + 1 , manipulator.worldPosition.z);
-
-            tPainter.RepaintAll();
-
         }
 
-    }
-
-
-    public void ClearSelection()
-    {
-        foreach (var manipulator in terrainVertexSelector.activeManipulators)
+        public void ClearSelection()
         {
-            Destroy(manipulator.frame.gameObject);
+            foreach (var manipulator in terrainVertexSelector.activeManipulators)
+            {
+                Destroy(manipulator.frame.gameObject);
+            }
+            terrainVertexSelector.activeManipulators.Clear();
         }
-        terrainVertexSelector.activeManipulators.Clear();
     }
 }
