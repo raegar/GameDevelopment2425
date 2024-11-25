c8856200fe7a03b86a2917e544bf5473ad2270ae
Jess <jw1519@student.aru.ac.uk>
2024-11-25 12:14:35 +0000

    added an add and remove invent

--- Assets/_Project/Scripts/Systems/vikingInventory/VikingInventory.cs
+++ Assets/_Project/Scripts/Systems/vikingInventory/VikingInventory.cs
@@ -10,6 +10,7 @@ namespace vikingInventory
     {
         public List<Item> itemList = new List<Item>();
         public TextMeshProUGUI text;
+        public int maxItemAmount = 5;
 
         public void Awake()
         {
@@ -19,9 +20,20 @@ namespace vikingInventory
         {
             foreach (var item in itemList)
             {
-                
                 Debug.Log(item.ToString());
             }
+        }
+        public void AddItem(Item item)
+        {
+            itemList.Add(item);
+            if (itemList.Count == maxItemAmount)
+            {
+                //put items in storage if invent is full
+            }
+        }
+        public void RemoveItem(Item item)
+        {
+            itemList.Remove(item);
         }
     }
 }
