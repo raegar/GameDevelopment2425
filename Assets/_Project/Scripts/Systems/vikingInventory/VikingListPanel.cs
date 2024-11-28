201ea578917527d9de68653d170a3af994d55a3b
JW1519 <JW1519@student.aru.ac.uk>
2024-11-28 11:50:29 +0000

    added script for VikingListPanel

---
+++ Assets/_Project/Scripts/Systems/vikingInventory/VikingListPanel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VikingListPanel : MonoBehaviour
+{
+    public void OpenVikingListpanel()
+    {
+
+    }
+    public void CloseVikingListpanel()
+    {
+
+    }
+    public void OpenVikingInventory()
+    {
+
+    }
+}
