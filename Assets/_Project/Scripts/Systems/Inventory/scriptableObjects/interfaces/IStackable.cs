47a36b677010e520cb3ba789bd450a37ec97ce8c
JW1519 <JW1519@student.aru.ac.uk>
2024-12-12 10:01:51 +0000

    added interface

---
+++ Assets/_Project/Scripts/Systems/Inventory/scriptableObjects/interfaces/IStackable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IStackable
+{
+    public void AddToStack(int amount); 
+    public void RemoveFromStack(int amount);
+}
