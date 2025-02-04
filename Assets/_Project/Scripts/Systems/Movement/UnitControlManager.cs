7456a2c7a7742b46af7ca77f54303e87ac236103
Donald MacSween <146818195+GameDevDon@users.noreply.github.com>
2025-02-04 17:19:36 +0000

    Settlement Resource Gathering (#29)
    * first experiment
    
    * Push of resource gathering / interactables
    
    * fix for inventory
    
    * Updated for animation wip
    
    * scene save
    
    * Better but inventory still causing problems
    
    * Last mod

--- Assets/_Project/Scripts/Systems/Movement/UnitControlManager.cs
+++ Assets/_Project/Scripts/Systems/Movement/UnitControlManager.cs
@@ -23,11 +23,18 @@ public class UnitControlManager : Singleton<UnitControlManager>
                 Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hitPoint;
 
+
                 if (Physics.Raycast(ray, out hitPoint))
                 {
-                    Vector3 finalPos = new Vector3(hitPoint.point.x, hitPoint.point.y, hitPoint.point.z);
-                    Order(finalPos);
-                    Debug.Log($"Mouse pos: {ray} Hitpoint pos: {hitPoint.point} Final pos: {finalPos}");
+                    if (hitPoint.collider.tag == "Interactable") 
+                    {
+                        hitPoint.collider.GetComponent<IInteractable>().Interact();
+                    }
+                    
+                        Vector3 finalPos = new Vector3(hitPoint.point.x, hitPoint.point.y, hitPoint.point.z);
+                        Order(finalPos);
+                        Debug.Log($"Mouse pos: {ray} Hitpoint pos: {hitPoint.point} Final pos: {finalPos}");
+                    
                 }
             }
         }
