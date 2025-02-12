0103a948b7c5be6c219b53bb73d1d8461e03834b
ID274 <ID274@student.aru.ac.uk>
2025-02-12 10:30:32 +0000

    Improved the population manager and viking tracker classes, and fixed missing references in demo scene

--- Assets/_Project/Scripts/Systems/VikingSpawner/PopulationManager.cs
+++ Assets/_Project/Scripts/Systems/VikingSpawner/PopulationManager.cs
@@ -1,6 +1,8 @@
 using PatternLibrary;
+using System.CodeDom.Compiler;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PopulationManager : Singleton<PopulationManager>
@@ -11,10 +13,10 @@ public class PopulationManager : Singleton<PopulationManager>
     [SerializeField] [Tooltip("Unchangeable (for the player) population limit")] private int trueMaxPopulation = 20;
 
     [Header("Population Tracking")]
-    public List<GameObject> population = new List<GameObject>();
+    public Dictionary<int, VikingTracker> population = new Dictionary<int, VikingTracker>();
     public int populationCount = 0;
 
-    public void AddMeToPopulation(GameObject vikingToAdd)
+    public void AddMeToPopulation(VikingTracker vikingToAdd)
     {
         if (populationCount >= currentMaxPopulation)
         {
@@ -23,14 +25,21 @@ public class PopulationManager : Singleton<PopulationManager>
         }
         else
         {
-            population.Add(vikingToAdd);
+            
+            if (vikingToAdd.GetID() == -1)
+            {
+                int ID = GenerateUniqueID();
+                vikingToAdd.SetID(ID);
+            }
+
+            population.Add(vikingToAdd.GetID(), vikingToAdd);
             populationCount++;
         }
     }
 
-    public void RemoveMeFromPopulation(GameObject vikingToRemove)
+    public void RemoveMeFromPopulation(int ID)
     {
-        population.Remove(vikingToRemove);
+        population.Remove(ID);
         populationCount--;
     }
 
@@ -38,5 +47,35 @@ public class PopulationManager : Singleton<PopulationManager>
     {
         currentMaxPopulation += amount;
         currentMaxPopulation = Mathf.Clamp(currentMaxPopulation, 1, trueMaxPopulation);
+    }
+
+    private int GenerateUniqueID()
+    {
+        int id = Random.Range(0, 10000);
+
+        while (population.ContainsKey(id))
+        {
+            id = Random.Range(0, 10000);
+        }
+
+        return id;
+    }
+
+    public VikingTracker FindVikingByID(int ID)
+    {
+        return population[ID];
+    }
+
+    public GameObject[] ReturnAllVikings()
+    {
+        GameObject[] vikingObjects = new GameObject[population.Count];
+        VikingTracker[] temp = population.Values.ToArray();
+
+        for (int i = 0; i < population.Count; i++)
+        {
+            vikingObjects[i] = temp[i].gameObject;
+        }
+
+        return vikingObjects;
     }
 }
