1e57aadd5272182fe1b4db2cc3f600e2d2e0491a
JW1519 <JW1519@student.aru.ac.uk>
2025-03-19 09:07:47 +0000

    added fail raid panel

--- Assets/_Project/Scripts/Systems/RaidSystem/RaidSystem.cs
+++ Assets/_Project/Scripts/Systems/RaidSystem/RaidSystem.cs
@@ -13,6 +13,7 @@ namespace raidSystem
     {
         public static RaidSystem instance;
         public VictoryPanel victoryPanel;
+        public RaidFailed FailPanel;
         public Transform vikingsInRaid;
         public Button raidPanelButton;
         public float raidLength = 60;
@@ -57,6 +58,7 @@ namespace raidSystem
                 {
                     Destroy(viking);
                 }
+                FailPanel.OpenPanel();
             }
             else
             {
